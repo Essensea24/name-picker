5eb5380d0ef8865b9b84dd50c09fb3d3887fe36f
Sophie Chor <sokpheakc@yahoo.com>
2015-04-30 17:02:30 -0700

    builds javascript picker

---
+++ app.js
@@ -0,0 +1,17 @@
+var wdi_16_names = ["Alexander Mireles", "Andre Trelles", "Andrew Ham", "Ari Kanamori", "Busyra Anwar", "Carlos Meza", "Chris Garvin", "Cristal Chavez", "Daley Meistrell", "Daniel Manuel", "Jefferson Choi", "Jessica Grinberg", "John Ward", "Kate Wood", "Kevin Mehrabi", "Max Tran", "Max Plisskin", "Megan Kakitsubo", "Meryl Harden", "Mike McCready", "Nick Dalke","Niels Bischoff", "Paul Aglione", "Rahul Sidhu", "Samuel Chun", "Samuel Amiri", "Sophie Chor","Steve Soter", "Taylor Adams", "Yo Wakita"]
+
+  var i = 0;
+    function eachName(){
+      console.log(wdi_16_names[i]);
+      i ++; 
+    }
+
+var namePicker = wdi_16_names[Math.floor(Math.random() * wdi_16_names.length)];
+console.log(namePicker);
+
+
+var namePicker = wdi_16_names[Math.floor(Math.random() * wdi_16_names.length)];
+console.log(namePicker);
+
+
+ 
